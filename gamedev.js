2c7b6994dab8e65d2a53531ed95501d6615d9546
210203 <210203@hkis.edu.hk>
2016-06-08 09:37:04 +0800

    Finished

--- gamedev.js
+++ gamedev.js
@@ -1,6 +1,31 @@
 $( document ).ready(function() {
 
-$( "#enemy1" ).click(function() {
-  $( "#enemy1" ).fadeOut( "slow" );
+$( ".enemy1" ).click(function() {
+  $( ".enemy1" ).fadeOut( "slow" );
 });
+$( ".enemy1" ).click(function() {
+  $( ".blood1" ).fadeIn( "slow" );
+});
+$(".blood1").hide();
+$( ".enemy2" ).click(function() {
+  $( ".enemy2" ).fadeOut( "slow" );
+});
+$( ".enemy2" ).click(function() {
+  $( ".blood2" ).fadeIn( "slow" );
+});
+$(".blood2").hide();
+$( ".enemy3" ).click(function() {
+  $( ".enemy3" ).fadeOut( "slow" );
+});
+$( ".enemy3" ).click(function() {
+  $( ".blood3" ).fadeIn( "slow" );
+});
+$(".blood3").hide();
+$( ".enemy4" ).click(function() {
+  $( ".enemy4" ).fadeOut( "slow" );
+});
+$( ".enemy4" ).click(function() {
+  $( ".blood4" ).fadeIn( "slow" );
+});
+$(".blood4").hide();
 });
